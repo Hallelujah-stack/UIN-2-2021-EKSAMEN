c6e712d0c6043444f92d9bacb6224a5c61a9cd62
StianFevang <81389801+StianFevang@users.noreply.github.com>
2021-04-27 12:35:23 +0200

    h

--- uin-base-setup/project/client/src/utils/client.js
+++ uin-base-setup/project/client/src/utils/client.js
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client';
 
 const options = {
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  projectId: process.env.jqdj8kih,
   dataset: process.env.REACT_APP_SANITY_DATASET_NAME,
   token: process.env.REACT_APP_SANITY_WRITE_TOKEN
 };
