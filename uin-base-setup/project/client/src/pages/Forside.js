3c91a5e52158e220e7e3f9d61271115061a9ae69
StianFevang <81389801+StianFevang@users.noreply.github.com>
2021-05-02 15:03:50 +0200

    title og lead
    title og lead komponent

--- uin-base-setup/project/client/src/pages/Forside.js
+++ uin-base-setup/project/client/src/pages/Forside.js
@@ -1,3 +1,11 @@
-const Forside = () => <h2>hei</h2>;
+import Lead from "../components/Lead";
+import Title from "../components/Title";
+
+const Forside = () => (
+    <container>
+    <Title title='Nyheter' />
+    <Lead lead="detter er ledeteksten til denne nettsiden" />
+    </container>
+);
 
 export default Forside;
