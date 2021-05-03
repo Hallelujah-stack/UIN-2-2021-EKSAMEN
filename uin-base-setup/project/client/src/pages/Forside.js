11d557c5713e27218afa7232bd6b96bff3df7ec3
malcolm <malcolm_appiah@yahoo.com>
2021-05-03 13:34:52 +0200

    Update Forside.js

--- uin-base-setup/project/client/src/pages/Forside.js
+++ uin-base-setup/project/client/src/pages/Forside.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import { Container, ScreenStyle, TitleH1 } from '../styles/Styles';
 import Screen from '../components/Screen';
 import Cards from '../components/Cards';
 import Card from '../components/Card';
+import Lead from '../components/Lead';
+import Title from '../components/Title';
+import { getArticle } from '../utils/articleService';
+
+const listCards = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchDataAsync = async () => {
+      setLoading(true);
+      try {
+        const Global = await getArticle('global');
+        setData(Global);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchDataAsync();
+  }, []);
+
+  if (loading) return <p>Loading ...</p>;
+  if (!data && error) return <p>{error?.message}</p>;
 
 const Forside = () => (
   <>
@@ -13,11 +40,15 @@ const Forside = () => (
       <ScreenStyle>
         <Screen />
       </ScreenStyle>
-      <Cards>
-        <Card />
-      </Cards>
+      <Title title={data?.title} />
+        <Lead lead={data?.lead} />
+        <Cards>
+          {data?.cards?.length > 0 &&
+            data.cards.map((card) => <Card key={card._key} {...card} />)}
+        </Cards>
     </Container>
   </>
 );
 
 export default Forside;
+
